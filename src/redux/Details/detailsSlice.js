1ce89bbd07cf0d1a01eb97276818a5cea9334414
isaacg <iwanyoike65@gmail.com>
2023-04-19 19:26:03 +0300

    Details changes

--- src/redux/Details/detailsSlice.js
+++ src/redux/Details/detailsSlice.js
@@ -1,4 +1,4 @@
-import {  createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
     countrydetails: [],
@@ -6,11 +6,35 @@ const initialState = {
     error: null,
 };
 
+const url = 'https://restcountries.com/v3.1/name/';
+
+export const fetchCountry = createAsyncThunk('details/fetchCountry', async (countryName, thunkAPI) => {
+    try {
+        const response = await fetch(`${url}${countryName}`);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        return thunkAPI.rejectWithValue(error.response.data);
+    }
+});
+
 const detailsSlice = createSlice({
     name: 'details',
     initialState,
     reducers: {},
     extraReducers: {
+        [fetchCountry.pending]: (state) => ({ ...state, isLoading: true }),
+        [fetchCountry.fulfilled]: (state, action) => ({
+            ...state,
+            isLoading: false,
+            countrydetails: action.payload,
+        }),
+        [fetchCountry.rejected]: (state, action) => ({
+            ...state,
+            isLoading: false,
+            error: action.payload,
+        }),
+
     },
 });
 
