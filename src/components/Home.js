4c2844baaa86452da0adb127ebfc23f5965e75af
isaacg <iwanyoike65@gmail.com>
2023-04-19 19:20:57 +0300

    Display all countries

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,62 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { fetchCountries } from '../redux/Home/homeSlice';
+
 const Home = () => {
+    const {
+        fetched, isLoading, countries,
+    } = useSelector((state) => state.home);
+    const dispatch = useDispatch();
+
+
+    useEffect(() => {
+        if (!fetched) {
+            dispatch(fetchCountries());
+        }
+    }, [dispatch, fetched]);
+
+    if (isLoading) {
+        return (
+            <p className={styles.loading}>Loading...</p>
+        );
+    }
 
     return (
         <div className="home">
-            <p>home</p>
+            <div >
+                <p >LIST OF COUNTRIES</p>
+            </div>
+            <ul>
+                {countries.map((country) => (
+                    <Link key={country.name.common} to={`/Details/${country.name.common}`}>
+                        <li key={country.name.common} >
+                            <span className={`material-symbols-outlined`}>
+                                arrow_circle_right
+                            </span>
+                            <div>
+                                <img src={country.flags.png} alt={country.flags.alt} />
+                            </div>
+                            <div>
+                                <p>
+                                    {country.name.common}
+                                </p>
+                                <p>
+                                    LAT:
+                                    {' '}
+                                    {country.latlng[0]}
+                                </p>
+                                <p>
+                                    LONG:
+                                    {' '}
+                                    {country.latlng[1]}
+                                </p>
+                            </div>
+                        </li>
+
+                    </Link>
+                ))}
+            </ul>
         </div>
     );
 };
