00365ebfa1b49096aead5a76476958bfd87c14b4
isaacg <iwanyoike65@gmail.com>
2023-04-19 19:31:19 +0300

    linter errors

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,61 +4,60 @@ import { Link } from 'react-router-dom';
 import { fetchCountries } from '../redux/Home/homeSlice';
 
 const Home = () => {
-    const {
-        fetched, isLoading, countries,
-    } = useSelector((state) => state.home);
-    const dispatch = useDispatch();
+  const {
+    fetched, isLoading, countries,
+  } = useSelector((state) => state.home);
+  const dispatch = useDispatch();
 
-
-    useEffect(() => {
-        if (!fetched) {
-            dispatch(fetchCountries());
-        }
-    }, [dispatch, fetched]);
-
-    if (isLoading) {
-        return (
-            <p className={styles.loading}>Loading...</p>
-        );
+  useEffect(() => {
+    if (!fetched) {
+      dispatch(fetchCountries());
     }
+  }, [dispatch, fetched]);
 
+  if (isLoading) {
     return (
-        <div className="home">
-            <div >
-                <p >LIST OF COUNTRIES</p>
-            </div>
-            <ul>
-                {countries.map((country) => (
-                    <Link key={country.name.common} to={`/Details/${country.name.common}`}>
-                        <li key={country.name.common} >
-                            <span className={`material-symbols-outlined`}>
-                                arrow_circle_right
-                            </span>
-                            <div>
-                                <img src={country.flags.png} alt={country.flags.alt} />
-                            </div>
-                            <div>
-                                <p>
-                                    {country.name.common}
-                                </p>
-                                <p>
-                                    LAT:
-                                    {' '}
-                                    {country.latlng[0]}
-                                </p>
-                                <p>
-                                    LONG:
-                                    {' '}
-                                    {country.latlng[1]}
-                                </p>
-                            </div>
-                        </li>
-
-                    </Link>
-                ))}
-            </ul>
-        </div>
+      <p>Loading...</p>
     );
+  }
+
+  return (
+    <div className="home">
+      <div>
+        <p>LIST OF COUNTRIES</p>
+      </div>
+      <ul>
+        {countries.map((country) => (
+          <Link key={country.name.common} to={`/Details/${country.name.common}`}>
+            <li key={country.name.common}>
+              <span className="material-symbols-outlined">
+                arrow_circle_right
+              </span>
+              <div>
+                <img src={country.flags.png} alt={country.flags.alt} />
+              </div>
+              <div>
+                <p>
+                  {country.name.common}
+                </p>
+                <p>
+                  LAT:
+                  {' '}
+                  {country.latlng[0]}
+                </p>
+                <p>
+                  LONG:
+                  {' '}
+                  {country.latlng[1]}
+                </p>
+              </div>
+            </li>
+
+          </Link>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
--- src/redux/store.js
+++ src/redux/store.js
@@ -3,10 +3,10 @@ import homeReducer from './Home/homeSlice';
 import detailsReducer from './Details/detailsSlice';
 
 const store = configureStore({
-    reducer: {
-        home: homeReducer,
-        details: detailsReducer,
-    },
+  reducer: {
+    home: homeReducer,
+    details: detailsReducer,
+  },
 });
 
 export default store;
