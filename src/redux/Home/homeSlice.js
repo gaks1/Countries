e2d4e549b8084a522f1f959d4439a9c1c5ddede7
isaacg <iwanyoike65@gmail.com>
2023-04-19 19:17:38 +0300

    Call api for all countries

--- src/redux/Home/homeSlice.js
+++ src/redux/Home/homeSlice.js
@@ -1,5 +1,6 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
+const url = 'https://restcountries.com/v3.1/all';
 
 const initialState = {
     countries: [],
@@ -8,11 +9,33 @@ const initialState = {
     error: null,
 };
 
+export const fetchCountries = createAsyncThunk('home/fetchCountries', async (_, thunkAPI) => {
+    try {
+        const response = await fetch(`${url}`);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        return thunkAPI.rejectWithValue(error.response.data);
+    }
+});
+
 const homeSlice = createSlice({
     name: 'home',
     initialState,
     reducers: {},
     extraReducers: {
+        [fetchCountries.pending]: (state) => ({ ...state, isLoading: true }),
+        [fetchCountries.fulfilled]: (state, action) => ({
+            ...state,
+            isLoading: false,
+            fetched: true,
+            countries: action.payload,
+        }),
+        [fetchCountries.rejected]: (state, action) => ({
+            ...state,
+            isLoading: false,
+            error: action.payload,
+        }),
     },
 
 });
