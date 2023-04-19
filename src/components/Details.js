f654ddd2e6ab33b42ac382b27ad403cc3b772281
isaacg <iwanyoike65@gmail.com>
2023-04-19 20:25:11 +0300

    Style the details component

--- src/components/Details.js
+++ src/components/Details.js
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { fetchCountry } from '../redux/Details/detailsSlice';
+import styles from './Details.module.css';
 import DetailsNavbar from './DetailsNavbar';
 
 const Details = () => {
@@ -15,22 +16,21 @@ const Details = () => {
 
   if (isLoading) {
     return (
-      <p>Loading...</p>
+      <p className={styles.loading}>Loading...</p>
     );
   }
   return (
     <div className="Details">
-      <h1>dvdv</h1>
       <DetailsNavbar />
       {countrydetails.map(
         (country) => (
           <div key={country.cca3}>
-            <div>
-              <div key={country.cca3}>
-                <div>
+            <div className={styles.divheader}>
+              <div key={country.cca3} className={styles.divtitle}>
+                <div className={styles.divtitleimg}>
                   <img src={country.flags.png} alt={country.flags.alt} />
                 </div>
-                <div>
+                <div className={styles.divtitletext}>
                   <h1>{country.name.common}</h1>
                   <p>
                     LAT:
@@ -45,89 +45,89 @@ const Details = () => {
                 </div>
               </div>
             </div>
-            <div>
-              <p>COUNTRY DETAILS</p>
+            <div className={styles.divseparator}>
+              <p className={styles.septext}>COUNTRY DETAILS</p>
             </div>
-            <ul>
-              <li key={country.cca3}>
-                <p>
-                  <span>Common name</span>
-                  <span>{country.name.common}</span>
+            <ul className={styles.ul}>
+              <li className={styles.liDetails} key={country.cca3}>
+                <p className={styles.pdetails}>
+                  <span className={styles.pstrong}>Common name</span>
+                  <span className={styles.pproperty}>{country.name.common}</span>
                 </p>
-                <p>
-                  <span>Official name</span>
-                  <span>{country.name.official}</span>
+                <p className={styles.pdetails}>
+                  <span className={styles.pstrong}>Official name</span>
+                  <span className={styles.pproperty}>{country.name.official}</span>
                 </p>
-                <p>
-                  <span>Capital</span>
-                  <span>
+                <p className={styles.pdetails}>
+                  <span className={styles.pstrong}>Capital</span>
+                  <span className={styles.pproperty}>
                     {Array.isArray(country.capital)
                       ? country.capital.join(',  ')
                       : country.capital}
                   </span>
                 </p>
-                <p>
-                  <span>Country region</span>
-                  <span>{country.region}</span>
+                <p className={styles.pdetails}>
+                  <span className={styles.pstrong}>Country region</span>
+                  <span className={styles.pproperty}>{country.region}</span>
                 </p>
-                <p>
-                  <span>Country-subregion</span>
-                  <span>{country.subregion}</span>
+                <p className={styles.pdetails}>
+                  <span className={styles.pstrong}>Country-subregion</span>
+                  <span className={styles.pproperty}>{country.subregion}</span>
                 </p>
-                <p>
-                  <span>Country-population</span>
-                  <span>{country.population}</span>
+                <p className={styles.pdetails}>
+                  <span className={styles.pstrong}>Country-population</span>
+                  <span className={styles.pproperty}>{country.population}</span>
                 </p>
-                <p>
-                  <span>Country-area</span>
-                  <span>{country.area}</span>
+                <p className={styles.pdetails}>
+                  <span className={styles.pstrong}>Country-area</span>
+                  <span className={styles.pproperty}>{country.area}</span>
                 </p>
-                <p>
-                  <span>Continent</span>
-                  <span>{country.continents}</span>
+                <p className={styles.pdetails}>
+                  <span className={styles.pstrong}>Continent</span>
+                  <span className={styles.pproperty}>{country.continents}</span>
                 </p>
-                <p>
-                  <span>{country.timezones.join(',  ')}</span>
+                <p className={styles.pdetails}>
+                  <span className={styles.pstrong}>Timezones</span>
+                  <span className={styles.pproperty}>{country.timezones.join(',  ')}</span>
                 </p>
-                <p>
-                  <span>Start of the week</span>
-                  <span>{country.startOfWeek}</span>
+                <p className={styles.pdetails}>
+                  <span className={styles.pstrong}>Start of the week</span>
+                  <span className={styles.pproperty}>{country.startOfWeek}</span>
                 </p>
-                <p>
-                  <span>Street view</span>
-                  <a href={country.maps.googleMaps}>google maps</a>
+                <p className={styles.pdetails}>
+                  <span className={styles.pstrong}>Street view</span>
+                  <a href={country.maps.googleMaps} className={styles.pproperty}>google maps</a>
                 </p>
-                <p>
-                  <span>Language:</span>
+                <p className={styles.pdetails}>
+                  <span className={styles.pstrong}>Language:</span>
                   {
-                                        country.languages && (
-                                        <ul>
-                                          {Object.entries(country.languages).map(([code, name]) => (
-                                            <li key={code}>{name}</li>
-                                          ))}
-                                        </ul>
-                                        )
-                                    }
+                    country.languages && (
+                      <ul className={styles.languageContainer}>
+                        {Object.entries(country.languages).map(([code, name]) => (
+                          <li key={code} className={styles.languageItem}>{name}</li>
+                        ))}
+                      </ul>
+                    )
+                  }
                 </p>
 
-                <p>
-                  <span>Currency:</span>
+                <p className={styles.pdetails}>
+                  <span className={styles.pstrong}>Currency:</span>
                   {
-                                        country.currencies && (
-                                        <ul>
-                                          {Object.entries(country.currencies)
-                                            .map(([code, currency]) => (
-                                              <li key={code}>
-                                                {currency.name}
-                                                {' '}
-                                                (
-                                                {currency.symbol}
-                                                )
-                                              </li>
-                                            ))}
-                                        </ul>
-                                        )
-                                    }
+                    country.currencies && (
+                      <ul className={styles.languageContainer}>
+                        {Object.entries(country.currencies).map(([code, currency]) => (
+                          <li key={code} className={styles.languageItem}>
+                            {currency.name}
+                            {' '}
+                            (
+                            {currency.symbol}
+                            )
+                          </li>
+                        ))}
+                      </ul>
+                    )
+                  }
                 </p>
 
               </li>
